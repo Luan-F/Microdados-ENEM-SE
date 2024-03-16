f976db11c3bf9cc23c0ce7f78a9d9ea61d6cd394
Felipe Santos <rfelipe273@gmail.com>
2024-03-16 12:05:53 -0300

    Corrige BuscarExemploQueryHandler que impedia projeto de rodar

--- MicrodadosEnemSergipe/MicrodadosEnemSergipe.Application/Exemplos/BuscarExemplo/BuscarExemploQueryHandler.cs
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.Application/Exemplos/BuscarExemplo/BuscarExemploQueryHandler.cs
@@ -3,34 +3,34 @@ using MicrodadosEnemSergipe.Domain.Abstractions;
 
 namespace MicrodadosEnemSergipe.Application.Exemplos.BuscarExemplo;
 
-internal sealed class GetBookingQueryHandler : IQueryHandler<BuscarExemploQuery, ExemploResponse>
+internal sealed class BuscarExemploQueryHandler : IQueryHandler<BuscarExemploQuery, ExemploResponse>
 {
-    private readonly dynamic _sqlConnectionFactory;
+    //private readonly dynamic _sqlConnectionFactory;
 
-    public GetBookingQueryHandler(dynamic sqlConnectionFactory)
-    {
-        _sqlConnectionFactory = sqlConnectionFactory;
-    }
+    //public BuscarExemploQueryHandler(dynamic sqlConnectionFactory)
+    //{
+    //    _sqlConnectionFactory = sqlConnectionFactory;
+    //}
 
     public async Task<Result<ExemploResponse>> Handle(BuscarExemploQuery request, CancellationToken cancellationToken)
     {
-        using var connection = _sqlConnectionFactory.CreateConnection();
+        //using var connection = _sqlConnectionFactory.CreateConnection();
 
-        const string sql = """
-            SELECT
-                id AS Id,
-                titulo AS Titulo,
-            FROM bookings
-            WHERE id = @ExemploId
-            """;
+        //const string sql = """
+        //    SELECT
+        //        id AS Id,
+        //        titulo AS Titulo,
+        //    FROM bookings
+        //    WHERE id = @ExemploId
+        //    """;
 
-        var exemplo = await connection.QueryFirstOrDefaultAsync<ExemploResponse>(
-            sql,
-            new
-            {
-                request.ExemploId
-            });
+        //var exemplo = await connection.QueryFirstOrDefaultAsync<ExemploResponse>(
+        //    sql,
+        //    new
+        //    {
+        //        request.ExemploId
+        //    });
 
-        return exemplo;
+        return Result.Success<ExemploResponse>(new ExemploResponse());
     }
 }
