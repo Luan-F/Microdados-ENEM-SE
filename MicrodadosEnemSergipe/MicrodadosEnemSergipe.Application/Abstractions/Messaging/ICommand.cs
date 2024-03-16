064dc857b4447dddd582554abe8506b77220fcce
Felipe Santos <rfelipe273@gmail.com>
2024-03-16 10:35:37 -0300

    Adiciona classes de Operation Result e abstrações para o Mediator

---
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MicrodadosEnemSergipe.Domain.Abstractions;
+
+namespace MicrodadosEnemSergipe.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+public interface ICommand<TReponse> : IRequest<Result<TReponse>>, IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
--- MicrodadosEnemSergipe/MicrodadosEnemSergipe.Domain/obj/Debug/net8.0/MicrodadosEnemSergipe.Domain.AssemblyInfo.cs
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.Domain/obj/Debug/net8.0/MicrodadosEnemSergipe.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicrodadosEnemSergipe.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef05f7d5fd22b9b89e26435544b7cebe0937dd1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicrodadosEnemSergipe.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicrodadosEnemSergipe.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
