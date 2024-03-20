e753d5f7f5590f0ff656bcf00d17f4c1acf64cae
Niceu Biriba <niceusantos1365@gmail.com>
2024-03-20 12:55:32 -0300

    feat: Mudança estrutura básica; Controller, Models, Viewdata; Connection Data

---
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Data/ContextConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MicrodadosEnemSergipe.WebApp.Data
+{
+    public class ContextConnection : DbContext
+    {
+        public ContextConnection(DbContextOptions<ContextConnection> options)
+           : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Participante> participante { get; set; }
+    }
+}
