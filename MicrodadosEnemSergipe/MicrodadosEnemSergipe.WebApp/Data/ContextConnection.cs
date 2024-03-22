919688bcf159d682defcfcdc798c7cb39b053926
Niceu Biriba <niceusantos1365@gmail.com>
2024-03-22 11:41:37 -0300

    feature/login-screen/12

--- MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Data/ContextConnection.cs
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Data/ContextConnection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Temporario;
 
 namespace MicrodadosEnemSergipe.WebApp.Data
 {
@@ -17,5 +18,6 @@ namespace MicrodadosEnemSergipe.WebApp.Data
         public DbSet<Redacao> redacao { get; set; }
         public DbSet<Escolaridade> escolaridade { get; set; }
         public DbSet<ProvaAreaConhecimento> provaareaconhecimento { get; set; }
+        public DbSet<Importacao> importacao { get; set; }
     }
 }
