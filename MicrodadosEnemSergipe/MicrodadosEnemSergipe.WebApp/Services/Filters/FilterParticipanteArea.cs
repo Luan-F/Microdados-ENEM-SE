bc0710a8468f80a567d8d1b880b09a488ee61a47
Luan-F <luan.leite@dcomp.ufs.br>
2024-03-23 18:34:17 -0300

    feat: Adicionando classe que filtra o partcipante com base na nota das áreas de conhecimento

---
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Services/Filters/FilterParticipanteArea.cs
@@ -0,0 +1,48 @@
+public class FilterParticipanteArea : BaseFilter {
+	private float[]? rangeCH, rangeCN, rangeMT, rangeLC;
+
+	public FilterParticipanteArea(float[]? rangeCH, float[]? rangeCN, float[]? rangeLC, float[]? rangeMT)
+	{
+		this.rangeCH = rangeCH;
+		this.rangeCN = rangeCN;
+		this.rangeMT = rangeMT;
+		this.rangeLC = rangeLC;
+	}
+
+	public override ParticipanteQueries Filter(ParticipanteQueries query)
+	{
+		if (rangeCH != null) {
+			query.provasAreaConhecimento = query
+				.provasAreaConhecimento.Where(
+						p => p.NotaCH >= rangeCH[0]
+						&& p.NotaCH <= rangeCH[1]);
+		}
+
+		if (rangeCN != null) {
+			query.provasAreaConhecimento = query
+				.provasAreaConhecimento.Where(
+						p => p.NotaCN >= rangeCN[0]
+						&& p.NotaCN <= rangeCN[1]);
+		}
+
+		if (rangeMT != null) {
+			query.provasAreaConhecimento = query
+				.provasAreaConhecimento.Where(
+						p => p.NotaMT >= rangeMT[0]
+						&& p.NotaMT <= rangeMT[1]);
+		}
+
+		if (rangeLC != null) {
+			query.provasAreaConhecimento = query
+				.provasAreaConhecimento.Where(
+						p => p.NotaLC >= rangeLC[0]
+						&& p.NotaLC <= rangeLC[1]);
+		}
+
+		if (next == null) {
+			return query;
+		}
+
+		return next.Filter(query);
+	}
+}
