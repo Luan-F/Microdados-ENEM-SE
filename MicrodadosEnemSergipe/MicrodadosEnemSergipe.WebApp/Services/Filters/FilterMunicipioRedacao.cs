1a1cc79cb1178969ba2925c2f304d2a509ca0c8c
Luan-F <luan.leite@dcomp.ufs.br>
2024-03-24 20:38:27 -0300

    feat: Adicionando classe que filtra o município com base nas notas da redação

---
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Services/Filters/FilterMunicipioRedacao.cs
@@ -0,0 +1,72 @@
+public class FilterMunicipioRedacao : BaseFilter {
+	private float[]? rangeComp1, rangeComp2, rangeComp3, rangeComp4, rangeComp5, rangeRedacao;
+
+	public FilterMunicipioRedacao(
+			float[]? rangeComp1, float[]? rangeComp2,
+			float[]? rangeComp3, float[]? rangeComp4,
+			float[]? rangeComp5, float[]? rangeRedacao
+		)
+	{
+		this.rangeComp1 = rangeComp1;
+		this.rangeComp2 = rangeComp2;
+		this.rangeComp3 = rangeComp3;
+		this.rangeComp4 = rangeComp4;
+		this.rangeComp5 = rangeComp5;
+		this.rangeRedacao = rangeRedacao;
+	}
+
+	public override AbstractQueryClass Filter(AbstractQueryClass query)
+	{
+		if (query.mediaMunicipios == null) {
+			throw new Exception("Campo mediaMunicipios é nulo");
+		}
+
+		if (rangeComp1 != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaComp1 >= rangeComp1[0]
+						&& p.NotaComp1 <= rangeComp1[1]);
+		}
+
+		if (rangeComp2 != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaComp2 >= rangeComp2[0]
+						&& p.NotaComp2 <= rangeComp2[1]);
+		}
+
+		if (rangeComp3 != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaComp3 >= rangeComp3[0]
+						&& p.NotaComp3 <= rangeComp3[1]);
+		}
+
+		if (rangeComp4 != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaComp4 >= rangeComp4[0]
+						&& p.NotaComp4 <= rangeComp4[1]);
+		}
+
+		if (rangeComp5 != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaComp5 >= rangeComp5[0]
+						&& p.NotaComp5 <= rangeComp5[1]);
+		}
+
+		if (rangeRedacao != null) {
+			query.mediaMunicipios = query
+				.mediaMunicipios.Where(
+						p => p.NotaRedacao >= rangeRedacao[0]
+						&& p.NotaRedacao <= rangeRedacao[1]);
+		}
+
+		if (next == null) {
+			return query;
+		}
+
+		return next.Filter(query);
+	}
+}
