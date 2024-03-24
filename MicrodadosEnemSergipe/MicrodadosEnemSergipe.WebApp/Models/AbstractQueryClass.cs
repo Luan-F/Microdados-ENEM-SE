fbbdc4af23373919b972ffacf5e85c34f0a23d2f
Luan-F <luan.leite@dcomp.ufs.br>
2024-03-24 20:18:40 -0300

    feat: Adicionando classe abstrata para as Queries

---
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Models/AbstractQueryClass.cs
@@ -0,0 +1,36 @@
+public abstract class AbstractQueryClass {
+	public IQueryable<ProvaAreaConhecimento>? provasAreaConhecimento;
+	public IQueryable<Redacao>? redacoes;
+	public IQueryable<Escolaridade>? escolaridades;
+	public IQueryable<Participante>? participantes;
+	public IQueryable<MediaMunicipio>? mediaMunicipios;
+
+	public void CopyFromAbstract(AbstractQueryClass queryClass)
+	{
+		this.provasAreaConhecimento = queryClass.provasAreaConhecimento;
+		this.redacoes = queryClass.redacoes;
+		this.escolaridades = queryClass.escolaridades;
+		this.participantes = queryClass.participantes;
+		this.mediaMunicipios = queryClass.mediaMunicipios;
+	}
+
+	public bool IsRedacoesNull()
+	{
+		return redacoes == null;
+	}
+
+	public bool IsEscolaridadesNull()
+	{
+		return escolaridades == null;
+	}
+
+	public bool IsParticipantesNull()
+	{
+		return participantes == null;
+	}
+
+	public bool IsProvasAreaConhecimentoNull()
+	{
+		return provasAreaConhecimento == null;
+	}
+}
