c6a93195a532aa10d4a08419f22e880588412a40
Luan-F <luan.leite@dcomp.ufs.br>
2024-03-24 20:18:01 -0300

    fix: Fazendo mudança para utilizar qualquer classe de Query

--- MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Interfaces/IFilter.cs
+++ MicrodadosEnemSergipe/MicrodadosEnemSergipe.WebApp/Interfaces/IFilter.cs
@@ -1,4 +1,4 @@
 public interface IFilter {
 	public void SetFilter(IFilter filter);
-	public ParticipanteQueries Filter(ParticipanteQueries query);
+	public AbstractQueryClass Filter(AbstractQueryClass query);
 }
